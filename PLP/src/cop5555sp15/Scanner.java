cf9baf4548be52338836999f9d0b35828b69351f
rprabhuh <rprabhu@ufl.edu>
2015-01-22 01:56:23 -0500

    Implemented the Scanner constructor and the scan function. "Empty Input" and "Only Spaces" test cases are passing.

--- PLP/src/cop5555sp15/Scanner.java
+++ PLP/src/cop5555sp15/Scanner.java
@@ -6,9 +6,21 @@ import static cop5555sp15.TokenStream.Kind.*;
 
 public class Scanner {
 
-
+	TokenStream stream;
+	FSA_STATES state;
+	int linecount,beg,end;
+	public static enum FSA_STATES{
+		START,
+		DIGIT,
+		IDENT,
+		COMMENT,
+	}
 	public Scanner(TokenStream stream) {
-		//IMPLEMENT THIS
+		this.stream = stream;
+		this.state = FSA_STATES.START;
+		this.linecount = 0;
+		this.beg = 0;
+		this.end = 0;
 	}
 
 
@@ -16,6 +28,55 @@ public class Scanner {
      //from the input
 	public void scan() {
           //IMPLEMENT THIS
+		for(int i=0;i<=stream.inputChars.length;i++) {
+			switch(state){
+			case START:
+				if(i==stream.inputChars.length) {
+					TokenStream.Token t = stream.new Token(EOF,beg,end,linecount);
+					stream.tokens.add(t);
+				} else if(Character.isWhitespace(stream.inputChars[i])) {
+					beg++;
+					end++;
+					if(stream.inputChars[i] == 10) {
+						//If the input char is LF, increment the linecount 
+						linecount++;
+					} else if (stream.inputChars[i] == 13) {
+						//If the input char is a CR, look ahead and see if you have a LF
+						//If the next char is a LF, increment i by 1
+						if(stream.inputChars[i+1] == 10) {
+							i++;
+							beg++;
+							end++;
+						}
+						//Increment linecount
+						linecount++;
+					}
+				} else if(stream.inputChars[i] == '/') {
+					beg++;
+					end++;
+					if(stream.inputChars[i+1]=='*')
+						state = FSA_STATES.COMMENT;
+						i = i+1;
+						beg++;
+						end++;
+				}
+				break;
+				
+			case COMMENT:
+				if(stream.inputChars[i] == '*' && stream.inputChars[i+1] == '/') {
+					state = FSA_STATES.START;
+					end = end + 2;
+				}
+				beg++;
+				break;
+			case DIGIT:
+				break;
+			case IDENT:
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
 }
